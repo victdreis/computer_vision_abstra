b07d5f77794884ddb34a07bf81e3d470a0eaf43e
victdreis <victoria.dias.reiss@gmail.com>
2025-02-10 13:02:44 -0300

    agent features

--- script_docvision.py
+++ script_docvision.py
@@ -1,37 +1,48 @@
 import json
-import abstra.forms as af
 from doc_vision.process_document import process_document
 import pdf2image
 from io import BytesIO
 from PIL import Image
 import tempfile
+from abstra.tasks import get_trigger_task, send_task
+import requests
+
+task = get_trigger_task()
 
 # Select document type
-document_type = af.read_dropdown(
-    "Selecione o tipo de documento:", 
-    options=[
-        "RG", "CPF", "CNH", "Certidão de Nascimento",
-        "Certidão de Casamento", "Comprovante de Endereço",
-        "CTPS", "Holerite", "Imposto de Renda", "FGTS"
-    ]
-)
+document_type = task['document_type']
 
-# File upload
-uploaded_file = af.read_file("Faça o upload do documento para processar:")
+# Select document extension
+document_extension = task['document_extension']
+
+# Upload file
+document_url = task['document_url']
+
+def get_document_content(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        document_content = response.content
+        return document_content
+    else:
+        print("Falha ao baixar o documento")
+        return None
+
+uploaded_file = get_document_content(document_url)
+
+if uploaded_file:
+    print("Documento obtido com sucesso")
 
 if uploaded_file:
     try:
         # Read file bytes
-        file_bytes = uploaded_file.file.read()
-        
-        # Check if it is a PDF
-        if uploaded_file.name.endswith(".pdf"):
+        file_bytes = uploaded_file
+
+        # Check if is pdf
+        if document_extension.endswith('pdf'):
             print("📄 Documento PDF detectado. Convertendo para imagem...")
 
-            # Convert the first page of the PDF to an image (JPG)
             images = pdf2image.convert_from_bytes(file_bytes, dpi=300)
 
-            # Save the first page as a temporary image
             with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_img:
                 images[0].save(temp_img.name, format="JPEG")
                 temp_img_path = temp_img.name 
@@ -54,7 +65,7 @@ if uploaded_file:
 
         # Debug logs
         print(f"""✅ DEBUG: Resultado final:
-{json.dumps(final_result, indent=4, ensure_ascii=False)}""")
+        {json.dumps(final_result, indent=4, ensure_ascii=False)}""")
 
         # Display results in the interface
         display_content = "<h3>Resultado:</h3>"
@@ -62,9 +73,22 @@ if uploaded_file:
         display_content += "<h3>Resultado Organizado:</h3>"
         display_content += f"<pre>{json.dumps(final_result['Informações Organizadas'], indent=4, ensure_ascii=False)}</pre>"
 
-        af.display_html(display_content)
+        print(display_content)
+
+        send_task("document_type", {
+            "visible_text" : final_result['Texto Visível'],
+            "organized_data": final_result['Informações Organizadas'],
+            **task.get_payload()
+        })
 
     except Exception as e:
         error_message = f"<span style='color: red;'>Erro ao processar: {e}</span>"
         print(f"❌ DEBUG: {error_message}")
-        af.display_html(error_message)
+        print(error_message)
+        
+        send_task("document_type", {
+            "error_message":f"Erro ao processar: {e}",
+            **task.get_payload()
+        })
+
+task.complete()
